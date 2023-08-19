364c2f1c467da557bb16546c59426841bf5c30cb
LamBao <baolam.nguyen2610@gmail.com>
2023-08-19 10:56:22 +0700

    column and lazy column

--- app/src/main/java/com/lambao/tutorial/MainActivity.kt
+++ app/src/main/java/com/lambao/tutorial/MainActivity.kt
@@ -4,36 +4,23 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -42,92 +29,50 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val snackbarHostState = remember { SnackbarHostState() }
-            val scope = rememberCoroutineScope()
-
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-
-            var basicTextFieldState by remember {
-                mutableStateOf("")
-            }
-
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                snackbarHost = { SnackbarHost(snackbarHostState) }
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(30.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    TextField(value = textFieldState, label = {
-                        Text(
-                            text = "Enter your name", modifier = Modifier.background(Color.Cyan)
-                        )
-                    }, singleLine = true, modifier = Modifier.fillMaxWidth(), onValueChange = {
-                        textFieldState = it
-                    })
-
-                    Spacer(modifier = Modifier.height(50.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            snackbarHostState.showSnackbar(
-                                message = "Hello $textFieldState",
-                                actionLabel = "Click me",
-                                duration = SnackbarDuration.Short
-                            )
-                        }
-                    }) {
-                        Text(text = "Show name!")
-                    }
-
-                    EditText(
-                        value = basicTextFieldState,
-                        onValueChanged = { basicTextFieldState = it },
-                        modifier = Modifier.fillMaxWidth().background(Color.LightGray).padding(16.dp)
+            LazyColumn {
+                itemsIndexed(
+                    listOf("Hello","World")
+                ) { index, item ->
+                    Text(
+                        text = "Item $index, value $item",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        textAlign = TextAlign.Center
                     )
-                    Spacer(modifier = Modifier.height(50.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            snackbarHostState.showSnackbar(
-                                message = "Hello $basicTextFieldState",
-                                actionLabel = "Click me",
-                                duration = SnackbarDuration.Short
-                            )
-                        }
-                    }) {
-                        Text(text = "Show name!")
-                    }
                 }
+                /*repeat(5000) {
+                    item {
+                        Text(
+                            text = "Item $it",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }*/
             }
-
-
         }
     }
 
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @Composable
-    fun ShowSnackBar() {
-        val snackbarHostState = remember { SnackbarHostState() }
-        val scope = rememberCoroutineScope()
-
-        Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }, content = {
-            Button(onClick = {
-                scope.launch {
-                    snackbarHostState.showSnackbar(
-                        message = "Hey this is a snackbar",
-                        actionLabel = "Click me",
-                        duration = SnackbarDuration.Short
-                    )
-                }
-            }) {
-                Text(text = "Click me")
+    fun Scroll() {
+        val state = rememberScrollState()
+        Column(
+            modifier = Modifier.verticalScroll(state),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            repeat(500) {
+                Text(
+                    text = "Item $it",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    textAlign = TextAlign.Center
+                )
             }
-        })
+        }
     }
 
     @Composable
