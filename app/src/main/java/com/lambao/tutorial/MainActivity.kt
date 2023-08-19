047e1f9569d3be6f2e2731755983f0c9dead85e8
LamBao <baolam.nguyen2610@gmail.com>
2023-08-19 14:27:44 +0700

    add constraint layout

--- app/src/main/java/com/lambao/tutorial/MainActivity.kt
+++ app/src/main/java/com/lambao/tutorial/MainActivity.kt
@@ -4,6 +4,19 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
 
@@ -11,7 +24,52 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constrains = ConstraintSet {
+                val greenBox = createRefFor("greenbox")
+                val redBox = createRefFor("redbox")
 
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.percent(0.2f)
+                    height = Dimension.ratio("1:1")
+
+//                    width = Dimension.percent(0.2f)
+//                    height = Dimension.percent(0.5f)
+
+//                    width = Dimension.value(100.dp)
+//                    height = Dimension.value(100.dp)
+                }
+
+                constrain(redBox) {
+                    top.linkTo(greenBox.bottom)
+//                    start.linkTo(parent.start)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+
+//                    width = Dimension.fillToConstraints
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.SpreadInside)
+            }
+
+            ConstraintLayout(
+                constrains,
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+//                        .width(200.dp)
+//                        .height(200.dp)
+                        .layoutId("greenbox")
+                )
+                Box(modifier = Modifier
+                    .background(Color.Red)
+                    .layoutId("redbox"))
+            }
         }
     }
 }
