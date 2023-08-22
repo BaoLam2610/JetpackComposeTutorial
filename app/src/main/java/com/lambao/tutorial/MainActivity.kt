d1a97fbb3244b2a8cecaef29fe16d7845c55f862
LamBao <baolam.nguyen2610@gmail.com>
2023-08-22 19:56:52 +0700

    launch effect

--- app/src/main/java/com/lambao/tutorial/MainActivity.kt
+++ app/src/main/java/com/lambao/tutorial/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.lambao.tutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val text by remember {
+                mutableStateOf("")
+            }
+            LaunchedEffect(key1 = text) {
+                delay(1000)
+                Log.d("lamnb", "This is: $text")
+            }
         }
     }
 }
