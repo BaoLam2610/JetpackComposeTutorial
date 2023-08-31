07afe5b0de95bfb2d3e6138132e7ac7d31e4993f
lamnb <lamnb.paraline>
2023-08-31 17:10:50 +0700

    add lazy grid

--- app/src/main/java/com/lambao/tutorial/MainActivity.kt
+++ app/src/main/java/com/lambao/tutorial/MainActivity.kt
@@ -3,13 +3,42 @@ package com.lambao.tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            val state = rememberLazyGridState(initialFirstVisibleItemIndex = 99)
+            LazyVerticalGrid(columns = GridCells.Adaptive(100.dp)  /* or Fixed(5) */,
+                state = state) {
+                items(100) {
+                    Box(
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(5.dp))
+                            .background(
+                                Color.LightGray
+                            )
+                    ) {
+                        Text(text = "Item $it", color = Color.Black)
+                    }
+                }
+            }
         }
     }
 }
