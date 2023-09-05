c18114e2d67329f05435bc13d09f7f10d7c45c91
lamnb <lamnb.paraline@gmail.com>
2023-09-05 14:24:36 +0700

    adjust color selected/unselected

--- app/src/main/java/com/lambao/tutorial/BottomNavigationDemo.kt
+++ app/src/main/java/com/lambao/tutorial/BottomNavigationDemo.kt
@@ -15,12 +15,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
@@ -62,6 +64,12 @@ fun NavigationBarDemo() {
                 label = {
                     Text(text = item.title)
                 },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Blue,
+                    unselectedIconColor = Color.Gray,
+                    selectedTextColor = Color.Red,
+                    indicatorColor = Color.Yellow
+                ),
                 icon = {
                     BadgedBox(badge = {
                         if (item.badgeCount != null)
